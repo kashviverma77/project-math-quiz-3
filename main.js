cdd594eaf7236c7c9fa6d60ef50a43d305483608
kashviverma77 <107224564+kashviverma77@users.noreply.github.com>
2022-07-26 10:18:49 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function send() {
+    number1 = document.getElementById("number1").value;
+    number2 = document.getElementById("number2").value;
+    acyual_answer = parseInt(number1) * parseInt(number2);
+}
